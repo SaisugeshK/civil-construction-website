fefe45b1faa9c6a9a8175bf1622778746df35fe1
Sai Sugesh Kumar K <saisugesh.k@uniquehire.co.in>
2026-08-18 06:49:19 +0530

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/civil-construction-website/',
   plugins: [react(), tailwindcss()],
 })
